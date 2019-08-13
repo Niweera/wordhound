7130e7b1f6a4226f2958859739e6d1ec89594c94
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-13 19:21:36 +0530

    final

--- create_env_file.sh
+++ create_env_file.sh
@@ -5,3 +5,11 @@ echo SECRET_OR_KEY=$SECRET_OR_KEY
 echo MONGO_URI=$MONGO_URI >> .env
 echo SECRET_OR_KEY=$SECRET_OR_KEY >> .env
 
+#echo env_variables: >> app.yaml
+#echo "  MONGO_URI: '$MONGO_URI'" >> app.yaml
+#echo "  SECRET_OR_KEY: '$SECRET_OR_KEY'" >> app.yaml
+cat <<EOF >> app.yaml
+env_variables:
+  MONGO_URI: "$MONGO_URI"
+  SECRET_OR_KEY: "$SECRET_OR_KEY"
+EOF
