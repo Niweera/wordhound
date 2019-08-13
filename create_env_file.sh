63329d4badb17f2cba8a7136dd079b4fbe8c9039
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-13 18:18:38 +0530

    test

--- create_env_file.sh
+++ create_env_file.sh
@@ -1,4 +1,6 @@
 #!/bin/bash                                                                                                                                                                                                                                                                    
 # Create a .env file from environment variables
+echo MONGO_URI=$MONGO_URI 
+echo SECRET_OR_KEY=$SECRET_OR_KEY 
 echo MONGO_URI=$MONGO_URI >> .env
 echo SECRET_OR_KEY=$SECRET_OR_KEY >> .env
