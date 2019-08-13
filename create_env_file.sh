669d19dd3ea4c893a3287231f55fbfcbec4b8492
root <root@nubuntu.global>
2019-08-13 11:59:02 +0000

    travis test

---
+++ create_env_file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                                                                                                                                                                                                    
+# Create a .env file from environment variables
+echo MONGO_URI=$MONGO_URI >> .env
+echo SECRET_OR_KEY=$SECRET_OR_KEY >> .env
